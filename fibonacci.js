e0dd0d98a047767bba7ed70c38e87227c46d6e98
leito <leonardobautista67@gmail.com>
2025-06-19 11:22:20 -0500

    commit

--- fibonacci.js
+++ fibonacci.js
@@ -1,20 +1,23 @@
-var numini = 0
-var numFin = 42
-let mapita = new Map ()
+var numini = 5
+var numFin = 21
+var mapita = new Map ([[1,numini],[2,numini+1],])
 let setsito = new Set ()
+
+
 var fibonacci = (numini,numFin) => {
     let a = numini
     let b = numini+1
     var list = [a,b];
-    let i = 0
-    mapita.set (i++,a)
+    var i = 3
+    // mapita.set(i++, a)
+    // mapita.set(i++, b)
     while(true){
         var siguiente = a + b
         if (siguiente > numFin) break 
         list.push(siguiente)
         a = b
         b = siguiente 
-        mapita.set (i++,b)
+        mapita.set (i++,siguiente)
     }
     return mapita
 }
@@ -37,13 +40,15 @@ console.log(fibonacc(numini,numFin));
 var fibonaccito = (numini,numFin) => {
     let a = numini
     let b = numini+1
-    var list = [a,b];
+    
+    setsito.add(a,b)
     while(true){
         var siguiente = a + b
         if (siguiente > numFin) break 
-        list.push(siguiente)
         a = b
         b = siguiente 
+        setsito.add(siguiente)
     }
-    return list
+    return setsito
 }
+console.log(fibonaccito(0,numFin))
